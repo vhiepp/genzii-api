5ccc30a1d23972caf06d0e222f079cc467b59797
Van Hiep Developer <dhiep2307@gmail.com>
2023-12-13 00:55:10 +0700

    update avatar model

--- app/Models/Avatar.php
+++ app/Models/Avatar.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 class Avatar extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'user_id',
+        'current'
+    ];
+
+    protected $hidden = [
+        'user_id'
+    ];
+
+    protected $dateFormat = 'U';
 }
