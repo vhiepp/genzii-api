8749dc5a495c3e09c42d3c8d2c27821f5b9aab89
Van Hiep Developer <dhiep2307@gmail.com>
2023-12-25 15:13:08 +0700

    update auth  middleware

--- routes/api.php
+++ routes/api.php
@@ -21,98 +21,102 @@ use App\Http\Controllers\HeartController;
 |
 */
 
-Route::middleware('api')->group(function () {
+//Route::middleware('api')->group(function () {
     Route::prefix('auth')->group(function () {
         Route::post('sign-in', [AuthController::class, 'signInWithEmailPassword']);
         Route::post('sign-in-with-firebase', [AuthController::class, 'signInWithFirebase']);
-        Route::get('sign-out', [AuthController::class, 'signOut']);
-        Route::get('profile', [AuthController::class, 'profile']);
-    });
+    });;
+    Route::middleware('auth')->group(function () {
+        Route::prefix('auth')->group(function () {
+            Route::get('sign-out', [AuthController::class, 'signOut']);
+            Route::get('profile', [AuthController::class, 'profile']);
+        });
 
-    Route::prefix('user')->group(function () {
+        Route::prefix('user')->group(function () {
 
-        Route::prefix('profile')->group(function () {
-            Route::post('', [UserController::class, 'profile']);
+            Route::prefix('profile')->group(function () {
+                Route::post('', [UserController::class, 'profile']);
+                Route::prefix('{id}')->group(function () {
+                    Route::get('', [UserController::class, 'profileWithId']);
+                });
+            });
+
             Route::prefix('{id}')->group(function () {
                 Route::get('', [UserController::class, 'profileWithId']);
+                Route::get('profile', [UserController::class, 'profileWithId']);
+                Route::get('posts', [PostController::class, 'getPostForUserId']);
+                Route::get('stories', [StoryController::class, 'getStoryForUserId']);
+            });
+
+            Route::prefix('search')->group(function () {
+                Route::post('', [UserController::class, 'searchUser']);
+            });
+
+        });
+
+        Route::prefix('friend')->group(function () {
+            Route::get('', [FriendController::class, 'friends']);
+
+            Route::post('', [FriendController::class, 'requestFriend']);
+            Route::post('send-request-friend', [FriendController::class, 'requestFriend']);
+            Route::delete('send-request-friend', [FriendController::class, 'cancelledSendRequestFriend']);
+
+            Route::delete('', [FriendController::class, 'cancelledFriend']);
+
+            Route::prefix('request')->group(function () {
+                Route::get('', [FriendController::class, 'friendRequests']);
+                Route::delete('', [FriendController::class, 'cancelledFriendRequests']);
+                Route::post('', [FriendController::class, 'agreedFriendRequests']);
             });
         });
 
-        Route::prefix('{id}')->group(function () {
-            Route::get('', [UserController::class, 'profileWithId']);
-            Route::get('profile', [UserController::class, 'profileWithId']);
-            Route::get('posts', [PostController::class, 'getPostForUserId']);
-            Route::get('stories', [StoryController::class, 'getStoryForUserId']);
+        Route::prefix('follower')->group(function () {
+            Route::get('', [FollowController::class, 'followers']);
+        });
+
+        Route::prefix('following')->group(function () {
+            Route::get('', [FollowController::class, 'following']);
+            Route::post('', [FollowController::class, 'followUser']);
+            Route::delete('', [FollowController::class, 'cancelledFollowUser']);
+        });
+
+        Route::prefix('posts')->group(function () {
+            Route::post('for-you', [PostController::class, 'getPosts']);
+            Route::post('', [PostController::class, 'createNewPost']);
+            Route::delete('', [PostController::class, 'deletePost']);
+
+            Route::prefix('{id}')->group(function () {
+                Route::get('', [PostController::class, 'getPostWithId']);
+
+                Route::prefix('comments')->group(function () {
+                    Route::get('', [CommentController::class, 'getCommentForPostId']);
+                    Route::post('', [CommentController::class, 'createCommentForPostId']);
+                });
+
+                Route::prefix('hearts')->group(function () {
+                    Route::post('', [HeartController::class, 'heartForPostId']);
+                });
+            });
+
+        });
+
+        Route::prefix('stories')->group(function () {
+            Route::post('', [StoryController::class, 'createNewStory']);
+            Route::get('users', [StoryController::class, 'getUserStoryListForYou']);
+            Route::get('', [StoryController::class, 'getStoryListForMe']);
+            Route::delete('', [StoryController::class, 'deleteStory']);
+
+            Route::prefix('{id}')->group(function () {
+                Route::get('', [StoryController::class, 'getStoryWithId']);
+            });
+        });
+
+        Route::prefix('comments')->group(function () {
+            Route::delete('', [CommentController::class, 'deleteComment']);
         });
 
         Route::prefix('search')->group(function () {
-            Route::post('', [UserController::class, 'searchUser']);
-        });
-
-    });
-
-    Route::prefix('friend')->group(function () {
-        Route::get('', [FriendController::class, 'friends']);
-
-        Route::post('', [FriendController::class, 'requestFriend']);
-        Route::post('send-request-friend', [FriendController::class, 'requestFriend']);
-        Route::delete('send-request-friend', [FriendController::class, 'cancelledSendRequestFriend']);
-
-        Route::delete('', [FriendController::class, 'cancelledFriend']);
-
-        Route::prefix('request')->group(function () {
-            Route::get('', [FriendController::class, 'friendRequests']);
-            Route::delete('', [FriendController::class, 'cancelledFriendRequests']);
-            Route::post('', [FriendController::class, 'agreedFriendRequests']);
+            Route::post('user', [UserController::class, 'searchUser']);
         });
     });
-
-    Route::prefix('follower')->group(function () {
-        Route::get('', [FollowController::class, 'followers']);
-    });
-
-    Route::prefix('following')->group(function () {
-        Route::get('', [FollowController::class, 'following']);
-        Route::post('', [FollowController::class, 'followUser']);
-        Route::delete('', [FollowController::class, 'cancelledFollowUser']);
-    });
-
-    Route::prefix('posts')->group(function () {
-        Route::post('for-you', [PostController::class, 'getPosts']);
-        Route::post('', [PostController::class, 'createNewPost']);
-        Route::delete('', [PostController::class, 'deletePost']);
-
-        Route::prefix('{id}')->group(function () {
-            Route::get('', [PostController::class, 'getPostWithId']);
-
-            Route::prefix('comments')->group(function () {
-                Route::get('', [CommentController::class, 'getCommentForPostId']);
-                Route::post('', [CommentController::class, 'createCommentForPostId']);
-            });
-
-            Route::prefix('hearts')->group(function () {
-                Route::post('', [HeartController::class, 'heartForPostId']);
-            });
-        });
-
-    });
-
-    Route::prefix('stories')->group(function () {
-        Route::post('', [StoryController::class, 'createNewStory']);
-        Route::get('users', [StoryController::class, 'getUserStoryListForYou']);
-        Route::get('', [StoryController::class, 'getStoryListForMe']);
-        Route::delete('', [StoryController::class, 'deleteStory']);
-
-        Route::prefix('{id}')->group(function () {
-            Route::get('', [StoryController::class, 'getStoryWithId']);
-        });
-    });
-
-    Route::prefix('comments')->group(function () {
-        Route::delete('', [CommentController::class, 'deleteComment']);
-    });
-
-    Route::prefix('search')->group(function () {
-        Route::post('user', [UserController::class, 'searchUser']);
-    });
-});
+//});
