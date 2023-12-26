83931f1ba500d7b9d120ab7f9a4cfba913ffa2ce
Van Hiep Developer <dhiep2307@gmail.com>
2023-12-27 06:52:23 +0700

    up

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 /*
@@ -15,5 +16,13 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function (\Illuminate\Http\Request $request) {
-
+    $posts = Post::whereIn('limit', ['all', 'friends'])->selectRaw('*, CAST(created_at AS UNSIGNED) AS created_at_number')->orderByDesc('created_at_number')->limit(500)->get();
+    foreach ($posts as $post) {
+        if (count($post->media) == 0) {
+            $post->media()->create([
+                'file_url' => \Vhiepp\VNDataFaker\VNFaker::image(600, 800, $post->caption),
+                'type' => 'image'
+            ]);
+        }
+    }
 });
