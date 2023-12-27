afce2ab4518ff5ee46632fca2a24b387a3e6b2bd
Van Hiep Developer <dhiep2307@gmail.com>
2023-12-27 07:04:14 +0700

    up

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function (\Illuminate\Http\Request $request) {
-    $posts = Post::whereIn('limit', ['all', 'friends'])->selectRaw('*, CAST(created_at AS UNSIGNED) AS created_at_number')->orderByDesc('created_at_number')->limit(50)->get();
+    $posts = Post::selectRaw('*, CAST(created_at AS UNSIGNED) AS created_at_number')->orderByDesc('created_at_number')->limit(50)->get();
     foreach ($posts as $post) {
         if (count($post->media) == 0) {
             $post->media()->create([
