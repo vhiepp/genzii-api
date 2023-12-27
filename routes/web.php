12ec43c2645edc32b858829181fb3183bbbcc359
Van Hiep Developer <dhiep2307@gmail.com>
2023-12-27 10:49:29 +0700

    fix post

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function (\Illuminate\Http\Request $request) {
     $count = 0;
-    $posts = Post::where('status', 'showing')->withCount('media')->orderBy('media_count', 'asc')->limit(50)->get();
+    $posts = Post::where('status', 'showing')->withCount('media')->orderBy('media_count', 'asc')->limit(10)->get();
     $ids = [];
     foreach ($posts as $post) {
         if ($post->media->count() == 0) {
